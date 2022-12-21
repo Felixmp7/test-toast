9efa6a7415dd7246d9d50a282b9aa3a82641b9d2
Felix Pacheco <devfex.19@gmail.com>
2022-12-21 11:24:35 -0400

    Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { TooltipAPI } from "react-tooltip-player";
 import "./App.css";
 
 function App() {
